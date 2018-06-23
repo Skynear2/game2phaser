8853332fb25c6343b60fad457c8ab45a2d09fa9e
Wesley Franco Ferreira <wesley2ff@gmail.com>
2018-06-23 20:29:36 -0300

    up

---
+++ src/BootState.js
@@ -0,0 +1,30 @@
+'use strict'
+
+class BootState extends Phaser.State {
+
+    preload() {
+        this.game.load.image('sky', 'assets/sky.png')
+        this.game.load.image('plane1', 'assets/airplane1.png')
+        this.game.load.image('shot', 'assets/shot.png')
+        this.game.load.image('wall', 'assets/wall.png')
+        this.game.load.image('fog', 'assets/fog.png')
+        this.game.load.image('saw', 'assets/saw.png')
+        this.game.load.image('smoke', 'assets/smoke.png')
+        this.game.load.image('title', 'assets/title.png')
+        this.game.load.spritesheet('vstick_button', 'assets/button_action.png', 50, 50)
+        this.game.load.spritesheet('vstick_dpad', 'assets/button_dpad.png', 105, 50)
+
+        this.game.load.spritesheet('explosion', 'assets/explosion.png', 56, 56)
+
+        // map
+        this.game.load.tilemap('level1', 'assets/map1.json', null, Phaser.Tilemap.TILED_JSON);
+       // this.game.load.tilemap('level2', 'assets/level2.json', null, Phaser.Tilemap.TILED_JSON)
+        this.game.load.image('Tiles_32x32','assets/Tiles_32x32.png');
+    }
+
+    create() {
+        console.log("BootState created")
+        // this.state.start('Title')
+        this.state.start('Game')
+    }
+}
